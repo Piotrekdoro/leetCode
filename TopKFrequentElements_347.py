dd9074331386a292248234f22f4bb2441d33d89f
Piotrekdoro <piotr.doroszko@o2.pl>
2023-07-13 23:43:40 +0200

    Finished TopKFrequentElements, started working on sudoku

--- TopKFrequentElements_347.py
+++ TopKFrequentElements_347.py
@@ -4,71 +4,40 @@ Given an integer array nums and an integer k, return the k most frequent element
 ''''''
 def topKFrequent(nums, k):
     numsDict={}
-    kBiggestNums=[]
+    kMostFreq=[]
 
     for i in range(len(nums)):
-        print('loop: '+str(i))
-        #print(kBiggestNums.count(nums[i]))
-        #print(bool(len(kBiggestNums)<k))
-        #print(bool(kBiggestNums.count(nums[i])!=0))
-        if kBiggestNums==[]:
-            kBiggestNums.append(nums[i])
-            numsDict[nums[i]]=1
-            print(nums[i])
-            print(numsDict)
-            print(kBiggestNums)
-        elif len(kBiggestNums)<k and kBiggestNums.count(nums[i])==0:
-            kBiggestNums.append(nums[i])
-            numsDict[nums[i]]=1
-            print(nums[i])
-            print(numsDict)
-            print(kBiggestNums)
-        elif len(kBiggestNums)<k and kBiggestNums.count(nums[i])!=0:
+        if nums[i] in numsDict:
             numsDict[nums[i]]+=1
-            print(nums[i])
-            print(numsDict)
-            print(kBiggestNums)
-        elif len(kBiggestNums)==k:
-            kBiggestNums.sort()
-            if nums[i]>kBiggestNums[0] and kBiggestNums.count(nums[i])==0:
-                del numsDict[kBiggestNums[0]]
-                kBiggestNums.pop(0)
-                kBiggestNums.append(nums[i])
-                numsDict[nums[i]]=1
-                print(nums[i])
-                print(numsDict)
-                print(kBiggestNums)
-            elif nums[i]>=kBiggestNums[0] and kBiggestNums.count(nums[i])!=0:
-                numsDict[nums[i]]+=1
-                print(nums[i])
-                print(numsDict)
-                print(kBiggestNums)
-    return kBiggestNums
+        else:
+            numsDict[nums[i]]=1
+    print(numsDict)
+    numsSorted=sorted(numsDict.items(),key=lambda numTouple:numTouple[1],reverse=True)
+    print(numsSorted)
+    for i in range(k):
+        kMostFreq.append(numsSorted[i][0])
+    return kMostFreq
 
-topKFrequent([2,5,2,2,3,4,3,8,5],3)
+print(topKFrequent([-1,-1],1))
+print(topKFrequent([1,1,1,2,2,3],2))
 
 '''
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         numsDict={}
-        kBiggestNums=[]
+        kMostFreq=[]
 
         for i in range(len(nums)):
-            if kBiggestNums==[]:
-                kBiggestNums.append(nums[i])
-                numsDict[nums[i]]=1
-            elif len(kBiggestNums)<k and kBiggestNums.count(nums[i])==0:
-                kBiggestNums.append(nums[i])
-                numsDict[nums[i]]=1
-            elif len(kBiggestNums)<k and kBiggestNums.count(nums[i])!=0:
+            if nums[i] in numsDict:
                 numsDict[nums[i]]+=1
-            elif len(kBiggestNums)==k:
-                kBiggestNums.sort()
-                if nums[i]>kBiggestNums[0] and kBiggestNums.count(nums[i])==0:
-                    del numsDict[kBiggestNums[0]]
-                    kBiggestNums.pop(0)
-                    kBiggestNums.append(nums[i])
-                    numsDict[nums[i]]=1
-                elif nums[i]>=kBiggestNums[0] and kBiggestNums.count(nums[i])!=0:
-                    numsDict[nums[i]]+=1
+            else:
+                numsDict[nums[i]]=1
+        print(numsDict)
+        numsSorted=sorted(numsDict.items(),key=lambda numTouple:numTouple[1],reverse=True)
+        print(numsSorted)
+        for i in range(k):
+            kMostFreq.append(numsSorted[i][0])
+        return kMostFreq
 '''
+
+
